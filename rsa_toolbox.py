e92df925d8c6f5ac9d4685dfef1734e515e7473a
FreeAhuraBird <131903454+FreeAhuraBird@users.noreply.github.com>
2025-02-03 13:22:36 +0100

    Update rsa_toolbox.py

--- rsa_toolbox.py
+++ rsa_toolbox.py
@@ -74,8 +74,8 @@ def calculate_modular_inverse(e, n):
         print(f"e: {e}, is not valid")
 
     message = 2350
-    encrypted_message = math.exp(message)%n
-    decrypted_message = math.exp(encrypted_message)%n
+    encrypted_message = (message**e)%n
+    decrypted_message = (encrypted_message**d)%n
     if(decrypted_message==message):
         print("({d},{n}) is the private key of ({e},{n}) public key")
 
